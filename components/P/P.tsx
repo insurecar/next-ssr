5e8d7faa650c52364b7ee1814c3763e62f814484
insurecar <rostyslavshyian@gmail.com>
2023-03-03 17:22:46 +0200

    added props

--- components/P/P.tsx
+++ components/P/P.tsx
@@ -3,12 +3,16 @@ import { PProps } from "./P.props";
 import styles from "./P.module.css";
 import cn from "classnames";
 
-export const P = ({ size = "s", children }: PProps): JSX.Element => {
+export const P = ({ size = "s", children, ...props }: PProps): JSX.Element => {
   const { p, s, m, l } = styles;
   const pClassNames = cn(p, {
     [s]: size === "s",
     [m]: size === "m",
     [l]: size === "l",
   });
-  return <p className={pClassNames}>{children}</p>;
+  return (
+    <p className={pClassNames} {...props}>
+      {children}
+    </p>
+  );
 };
