c0a8f0c795749e58c67ccc0f736ef0706c43f3b1
insurecar <rostyslavshyian@gmail.com>
2023-03-03 14:44:44 +0200

    finished 40 added button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Htag } from "../components/Htag/Htag";
+import { Button } from "../components/Button/Button";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h3">text</Htag>
+      <Button appearence="ghost">qwer</Button>
     </div>
   );
 }
