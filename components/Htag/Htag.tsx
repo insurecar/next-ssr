2887191f3f4f008dd33574da12d36ef9519d119c
insurecar <rostyslavshyian@gmail.com>
2023-03-03 13:13:47 +0200

    added spaces

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -5,11 +5,13 @@ import cn from "classnames";
 
 export const Htag = ({ tag, children }: HtagProps): JSX.Element => {
   const { h1, h2, h3 } = styles;
+
   const htagClasss = cn("", {
     [h1]: tag === "h1",
     [h2]: tag === "h2",
     [h3]: tag === "h3",
   });
+
   switch (tag) {
     case "h1":
       return <h1 className={htagClasss}>{children}</h1>;
