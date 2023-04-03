bb1a1cd1054fe2dbdaa18cdc077386f6a82b1417
insurecar <rostyslavshyian@gmail.com>
2023-04-03 15:10:47 +0300

    finished 80

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import cn from "classnames";
 import { TopPageComponentProps } from "./TopPageComponent.props";
+import { Htag, Tag } from "@/components";
+import styles from "./TopPageComponent.module.css";
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <div>{products && products.length}</div>;
+  return (
+    <div className={styles.title}>
+      <Htag tag="h1">{page.title}</Htag>
+
+      {products && (
+        <Tag color="gray" size="m">
+          {products.length}
+        </Tag>
+      )}
+      <span> Сортування</span>
+    </div>
+  );
 };
