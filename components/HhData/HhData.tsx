b20376f13cfffe5b0b4ee26ddf92b47c312c0816
insurecar <rostyslavshyian@gmail.com>
2023-04-04 15:16:34 +0300

    finished 81

---
+++ components/HhData/HhData.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "./HhData.module.css";
+import cn from "classnames";
+import { HhDataProps } from "./HhData.props";
+import { Card } from "..";
+import RateIcon from "./rate.svg";
+
+export const HhData = ({
+  count,
+  juniorSalary,
+  middleSalary,
+  seniorSalary,
+}: HhDataProps): JSX.Element => {
+  return (
+    <div className={styles.hh}>
+      <Card className={styles.count}>
+        <div className={styles.title}>Всього вакансій</div>
+        <div className={styles.countValue}>{count}</div>
+      </Card>
+      <Card className={styles.salary}>
+        <div>
+          <div className={styles.title}>Навчальний</div>
+          <div className={styles.salaryValue}>{juniorSalary}</div>
+          <div className={styles.rate}>
+            <RateIcon className={styles.filled} />
+            <RateIcon />
+            <RateIcon />
+          </div>
+        </div>
+
+        <div>
+          <div className={styles.title}>Середній</div>
+          <div className={styles.salaryValue}>{middleSalary}</div>
+          <div className={styles.rate}>
+            <RateIcon className={styles.filled} />
+            <RateIcon className={styles.filled} />
+            <RateIcon />
+          </div>
+        </div>
+
+        <div>
+          <div className={styles.title}>Професіонал</div>
+          <div className={styles.salaryValue}>{seniorSalary}</div>
+          <div className={styles.rate}>
+            <RateIcon className={styles.filled} />
+            <RateIcon className={styles.filled} />
+            <RateIcon className={styles.filled} />
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import cn from "classnames";
 import { TopPageComponentProps } from "./TopPageComponent.props";
-import { Htag, Tag } from "@/components";
+import { Card, HhData, Htag, Tag } from "@/components";
 import styles from "./TopPageComponent.module.css";
+import { TopLevelCategory } from "@/interfaces/page.interface";
 
 export const TopPageComponent = ({
   page,
@@ -10,15 +11,27 @@ export const TopPageComponent = ({
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
   return (
-    <div className={styles.title}>
-      <Htag tag="h1">{page.title}</Htag>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
 
-      {products && (
-        <Tag color="gray" size="m">
-          {products.length}
+        {products && (
+          <Tag color="green" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <span> Сортування</span>
+      </div>
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансії - {page.category}</Htag>
+        <Tag color="red" size="m">
+          hh.ua
         </Tag>
-      )}
-      <span> Сортування</span>
+      </div>
+      {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
     </div>
   );
 };
